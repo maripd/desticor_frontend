6afc1611337044e74467c13c36e6235192df18d5
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2023-03-24 22:18:29 -0700

    login page mobile view
    started mobile view of login page

--- client/src/pages/LoginView.jsx
+++ client/src/pages/LoginView.jsx
@@ -1,5 +1,22 @@
+import "../styles/login.css";
 import React from "react";
 const LoginView = () => {
-  return <div>login page</div>;
+  return (
+    <>
+      <div className="bgc">
+        <form className="card">
+          <div className="info">
+            <input type="text" placeholder="username" />
+            <input type="text" name="password" id="pw" placeholder="password" />
+          </div>
+
+          <div className="btn">
+            <a href="/">Submit</a>
+            <a href="/landingpage">Cancel</a>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 };
 export default LoginView;

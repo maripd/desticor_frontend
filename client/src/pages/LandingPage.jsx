3acc5b77b486364a310eda91339df5cfbacabce8
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2023-03-24 21:24:46 -0700

    basic landing page placeholder

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,6 +1,23 @@
 import "../styles/landingPage.css";
 import React from "react";
 const LandingPage = () => {
-  return <div>landing page</div>;
+  return (
+    <>
+      <div id="bgc">
+        <div className="intro">
+          <h1>Desticor</h1>
+          <p>Explore the World</p>
+          <p>Anytime, Anywhere</p>
+        </div>
+
+        <div className="over">
+          <button className="logbtn">Login</button>
+          <a href="/register" id="reg">
+            create an account
+          </a>
+        </div>
+      </div>
+    </>
+  );
 };
 export default LandingPage;

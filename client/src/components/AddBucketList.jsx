633c2f0d0ffce3d2d107b20be611220d919f9f3c
Marissa Dilig <hello.maripd@gmail.com>
2023-03-27 09:37:48 -0500

    updated addbucketlist component

--- client/src/components/AddBucketList.jsx
+++ client/src/components/AddBucketList.jsx
@@ -1,11 +1,25 @@
 import './addbucketlist.css'
-const AddBucketList = () => {
+import { useState} from 'react'
+import axios from 'axios'
 
+
+const AddBucketList = () => {
+  const [bucketListNameData, setBucketListName] = useState('')
+
+const handleSubmit = async (e) => {
+  e.preventDefault()
+  console.log('submit button was clicked!')
+  const response = await axios.post( '/createbucketlist', {
+    bucketListName: bucketListNameData,
+  })
+  setBucketListName()
+  console.log('')
+}
   return (
     <>
       <form id="addbucketform-container">
-        <input type="text" placeholder="name of bucket list" id="addbucket-input"/>
-        <button type="submit" id="submit-bucket">Submit</button>
+        <input type="text" value={bucketListNameData} placeholder="name of bucket list" id="addbucket-input"/>
+        <button type="submit" id="submit-bucket" onClick={handleSubmit}>Submit</button>
       </form>
     </>
   )

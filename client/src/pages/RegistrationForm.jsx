ac5f7d27d38bb41cb04c1a22d3cc065f445b71c7
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2023-03-27 16:20:54 -0700

    Update RegistrationForm.jsx

--- client/src/pages/RegistrationForm.jsx
+++ client/src/pages/RegistrationForm.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import "../styles/login.css";
 const RegistrationForm = () => {
-  return <div>registration page</div>;
+  return (
+    <>
+      <div className="bgc">
+        <form className="card">
+          <div className="info">
+            <input type="text" placeholder="first name" />
+            <input type="text" placeholder="last name" />
+            <input type="text" placeholder="username" />
+            <input type="text" placeholder="email" />
+            <input type="text" name="password" id="pw" placeholder="password" />
+          </div>
+
+          <div className="btn">
+            <a href="/">Submit</a>
+            <a href="/landingpage">Cancel</a>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 };
 export default RegistrationForm;

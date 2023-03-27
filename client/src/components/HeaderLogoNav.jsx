b8921c3e922700da92f82b4b4e358f5d355443b7
Marissa Dilig <hello.maripd@gmail.com>
2023-03-27 01:24:54 -0500

    home and back button are now working

--- client/src/components/HeaderLogoNav.jsx
+++ client/src/components/HeaderLogoNav.jsx
@@ -1,10 +1,21 @@
 import './headerlogonav.css'
+import { useNavigate } from 'react-router-dom'
 
 const HeaderLogoNav = () => {
+  const navigate = useNavigate()
+
+  const handleHomeButton = (e) => {
+  navigate('/')
+  }
+
+  const handleBackButton = (e) => {
+    navigate(-1)
+  }
+
   return (
     <div id="nav-container">
-        <img src="https://i.imgur.com/pPRVGLB.png" className="logo-icon" />
-        <img src="https://i.imgur.com/mnVheUL.png" className="nav-icon" />
+        <img src="https://i.imgur.com/pPRVGLB.png" className="logo-icon" onClick={handleHomeButton}/>
+        <img src="https://i.imgur.com/mnVheUL.png" className="nav-icon" onClick={handleBackButton}/>
       </div>
   )
 }

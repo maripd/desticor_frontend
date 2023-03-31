369e0e38ee93f3e44ae913ebc211e4cd59b66f50
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2023-03-31 09:19:33 -0700

    Update ReviewView.jsx

--- client/src/pages/ReviewView.jsx
+++ client/src/pages/ReviewView.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 const ReviewView = () => {
-  return <div>review page</div>;
+  return (
+    <>
+      <div>review page background image here</div>
+      <div>
+        destination name here
+        <form>
+          <textarea></textarea>
+        </form>
+      </div>
+    </>
+  );
 };
 export default ReviewView;

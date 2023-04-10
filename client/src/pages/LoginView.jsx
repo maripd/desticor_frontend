9f08ab92cea474fa2dd46adcdaddf47a65eff914
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2023-04-09 17:54:12 -0700

    form validation for login & register

--- client/src/pages/LoginView.jsx
+++ client/src/pages/LoginView.jsx
@@ -6,55 +6,113 @@ import axios from "axios";
 const LoginView = () => {
   const [emailAddress, setEmailAddress] = useState("");
   const [password, setPassword] = useState("");
+  const [loginError, setLoginError] = useState("");
+  const [login, setLogin] = useState(false);
 
   const navigate = useNavigate();
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    axios
-      .post("http://localhost:3001/createuser", {
+    try {
+      const response = await axios.post("http://localhost:3001/login", {
         emailAddress,
         password
-      })
-      .then((result) => navigate("/"))
-
-      .catch((error) => {
-        console.error(error);
       });
+      if (response.status === 200) {
+        setLogin(true);
+        navigate("/");
+      }
+    } catch (error) {
+      setLoginError("Invalid email or password");
+      console.log(error);
+    }
   };
 
   return (
     <>
-      <div className="bgc">
-        <form className="card" onSubmit={handleSubmit}>
-          <div className="info">
-            <input
-              type="email"
-              name="emailAddress"
-              value={emailAddress}
-              onChange={(e) => setEmailAddress(e.target.value)}
-              placeholder="email"
-              className="login-input"
-            />
-            <input
-              type="password"
-              name="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="password"
-              className="login-input"
-            />
-          </div>
+      {login ? (
+        <div className="bgc">
+          <form className="card" onSubmit={handleSubmit}>
+            <div className="info">
+              <input
+                type="email"
+                name="emailAddress"
+                value={emailAddress}
+                onChange={(e) => setEmailAddress(e.target.value)}
+                placeholder="email"
+                className="login-input"
+              />
+              <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="password"
+                className="login-input"
+              />
+            </div>
 
-          <div className="btn">
-            <button onClick={(e) => handleSubmit(e)} className="login-btn">Log In</button>
+            <div className="btn">
+              <button onClick={(e) => handleSubmit(e)} className="login-btn">
+                Log In
+              </button>
 
-            <button onClick={() => navigate("/landingpage")} className="login-btn">Cancel</button>
-          </div>
-          <p className="login-text">
-              <Link to="/register" className="login-url">Create an Account</Link>
+              <button
+                onClick={() => navigate("/landingpage")}
+                className="login-btn"
+              >
+                Cancel
+              </button>
+            </div>
+            <p className="login-text">
+              <Link to="/register" className="login-url">
+                Create an Account
+              </Link>
             </p>
-        </form>
-      </div>
+          </form>
+        </div>
+      ) : (
+        <div className="bgc">
+          <p>{loginError}</p>
+          <form className="card" onSubmit={handleSubmit}>
+            <div className="info">
+              <input
+                type="email"
+                name="emailAddress"
+                value={emailAddress}
+                onChange={(e) => setEmailAddress(e.target.value)}
+                placeholder="email"
+                className="login-input"
+              />
+              <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="password"
+                className="login-input"
+              />
+            </div>
+
+            <div className="btn">
+              <button onClick={(e) => handleSubmit(e)} className="login-btn">
+                Log In
+              </button>
+
+              <button
+                onClick={() => navigate("/landingpage")}
+                className="login-btn"
+              >
+                Cancel
+              </button>
+            </div>
+            <p className="login-text">
+              <Link to="/register" className="login-url">
+                Create an Account
+              </Link>
+            </p>
+          </form>
+        </div>
+      )}
     </>
   );
 };

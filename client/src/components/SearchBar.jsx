4111c2596936563626f5b0cb8de543b3d472e53a
Mari PD <48144818+maripd@users.noreply.github.com>
2023-04-13 00:39:05 -0500

    added functionalities for adding bucket list card, bucket list dropdown panel

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,20 +1,31 @@
 import '../styles/searchbar.css'
 import { useState } from 'react'
+import axios from 'axios'
+
 const google = window.google
 const autocompleteService = new google.maps.places.AutocompleteService()
-const placesService = new google.maps.places.PlacesService(document.createElement('div'))
+const placesService = new google.maps.places.PlacesService(
+  document.createElement('div')
+)
 
-const SearchBar = () => {
+const SearchBar = ({ bucketList }) => {
+  // props { bucketlist: [{ bucketlistName: '' }, { bucketListName: '' }], something: ''   }
+  // props.bucketList.map()
+  // props.something
+  // or bucketList.map()
+  // or something
+
   const [inputValues, setInput] = useState('')
   const [predictions, setPredictions] = useState([])
   const [photo, setPhoto] = useState(null)
+  const [cityData, setCityData] = useState(null)
 
   const handleChange = (e) => {
     console.log(e.target.value)
     setInput(e.target.value)
     // get a list from google, array of strings
     autocompleteService.getPlacePredictions(
-      { input: e.target.value, types: ['(cities)']},
+      { input: e.target.value, types: ['(cities)'] },
       (predictions) => {
         console.log(predictions)
         // if predictions value is not null, then set the predictions
@@ -31,19 +42,22 @@ const SearchBar = () => {
 
   const handleCityClick = (placeId) => {
     placesService.getDetails(
-      { placeId: placeId, fields: ['name', 'photos']},
+      { placeId: placeId, fields: ['name', 'photos'] },
       (place, status) => {
-        if (status === google.maps.places.PlacesServiceStatus.OK && place.photos && place.photos.length > 0) {
+        if (
+          status === google.maps.places.PlacesServiceStatus.OK &&
+          place.photos &&
+          place.photos.length > 0
+        ) {
           const photoUrl = place.photos[0].getUrl({
-            maxWidth: 500,
-            maxHeight: 600,
+            maxWidth: 250,
+            maxHeight: 300
           })
           setPhoto(photoUrl)
         }
       }
     )
   }
-
 
   return (
     <div id="blurbsearch-container">
@@ -77,11 +91,41 @@ const SearchBar = () => {
             })}
           </div>
         )}
-        {photo && <img src={photo} alt="city photo" />}
+        {photo && (
+          <div id="photodata-container">
+            <form className="bucketlistname-dropdown">
+              <h6 className="bucket-title">Buckets...</h6>
+              <img
+                src="https://i.imgur.com/Gl9m7pe.png"
+                className="dropdown-icon"
+              />
+              <button id="savebucket-btn">Save</button>
+            </form>
+            <img src={photo} alt="city photo" id="photo-data" />
+
+            <ul className="bucketdropdown-panel">
+              {bucketList.map((bucketItem) => {
+                return (
+                  <li className="dropdown-list">
+                    <img
+                      src={photo}
+                      className="dropdown-img"
+                    />
+                    <h6 className="dropdown-text">{bucketItem.bucketListName}</h6>
+                    
+                    <div className="dropdownsave-container">
+                    <button className="dropdown-save">Save</button>
+
+                    </div>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        )}
+        {cityData && <div id="cityname-data">{setCityData}</div>}
       </div>
     </div>
   )
 }
 export default SearchBar
-
-
